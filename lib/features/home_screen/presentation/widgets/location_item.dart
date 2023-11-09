b521f000d4b3893ef1d881578f9218520be76196
HatemReda7 <hreda2567@gmail.com>
2023-11-09 16:19:44 +0200

    removed warnings

--- lib/features/home_screen/presentation/widgets/location_item.dart
+++ lib/features/home_screen/presentation/widgets/location_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:weather/core/api_manager/api_functions.dart';
 import 'package:weather/core/utils/styles.dart';
